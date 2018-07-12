331dfc8d9b53aaea747a5eef5663942623fd52f0
roly hudson <roly.hudson@gmail.com>
2018-07-12 14:26:51 -0500

    added intial spark tests

---
+++ sparkKmeans/src/main/java/org/rolson/emr/sparkKmeans/App.java
@@ -0,0 +1,13 @@
+package org.rolson.emr.sparkKmeans;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
